bea0c02d92052d5b058be6578fa8654ff070d48d
prasannapujari <prasanna.pujari66@gmail.com>
2022-03-13 00:18:05 -0500

    t

---
+++ src/main/java/ro/robertsicoie/appengine/Application.java
@@ -0,0 +1,60 @@
+package ro.robertsicoie.appengine;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ro.robertsicoie.appengine.daos.UserDao;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.logging.Logger;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@Autowired()
+	@Qualifier("datastoreUserDao")
+	private UserDao userDao;
+	private static final Logger logger = Logger.getLogger("My App");
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/")
+	public String root(HttpServletRequest request) {
+
+		User user = new User();
+		user.setIp(request.getRemoteAddr());
+		user.setDate(new Date(request.getSession().getCreationTime()));
+		user.setAgent(request.getHeader("User-Agent"));
+		userDao.add(user);
+
+		StringBuilder response = new StringBuilder();
+		userDao.getUsers().forEach(u -> response.append(u).append("<br/>"));
+		return response.toString();
+	}
+
+	@GetMapping(value="/app")
+	public String  testApp(){
+		logger.info("NNNNNNNNNNNNNNNNNNNNNN");
+		ReplyToEmail replyToEmail=new ReplyToEmail();
+		replyToEmail.callEmail();
+		logger.info("NNNNNNNNNNNNNNNNNNNNNN");
+		return "test";
+	}
+
+	@Scheduled(fixedRate = 100)
+	public String firstApp(){
+		//logger.info("NNNNNNNNNNNNNNNNNNNNNN");
+		ReplyToEmail replyToEmail=new ReplyToEmail();
+		replyToEmail.callEmail();
+
+		//logger.info("NNNNNNNNNNNNNNNNNNNNNN");
+		return  "Yes";
+	}
+}
---
+++ src/main/java/ro/robertsicoie/appengine/ReplyToEmail.java
@@ -0,0 +1,171 @@
+package ro.robertsicoie.appengine;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.search.FlagTerm;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.Properties;
+
+public class ReplyToEmail {
+    public  boolean sendEmailStatus=false;
+    public SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss.SSS");
+    public static String from = "prasanna.pujari66@gmail.com";
+    public static String pass ="xyboiirowplwsbrn";
+    // Recipient's email ID needs to be mentioned.
+    public static String host = "smtp.gmail.com";
+    public static ReplyToEmail replyToEmail=new ReplyToEmail();
+
+    public static  Session session;
+
+    public String  toEmail;
+    public Folder inbox ;
+    public Message[] messages;
+    public String subject;
+
+   /* public static void main(String[] args) {
+       // ba68fc71-073b-4da1-994a-4fd915d66c49
+
+       *//* ReplyToEmail replyToEmail=new ReplyToEmail();
+        replyToEmail.callEmail();*//*
+    }*/
+
+    public void callEmail(){
+        // Get system properties
+        Properties properties = System.getProperties();
+        // Setup mail server
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.user", from);
+        properties.put("mail.smtp.password", pass);
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+
+        // Get the default Session object.
+        session = Session.getDefaultInstance(properties);
+        // create the imap store object and connect to the imap server
+
+        try{
+            Store store = session.getStore("imaps");
+            store.connect(host, from, pass);
+           /* while (!replyToEmail.sendEmailStatus) {*/
+                replyToEmail.checkInbox(store);
+           /* }
+            if(replyToEmail.sendEmailStatus){*/
+                store.close();
+            //}
+        }catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+    public  void   checkInbox(Store store ){
+        // Sender's email ID needs to be mentioned
+       // System.out.println("----"+formatter.format(Calendar.getInstance().getTime())+"-----------");
+        try{
+
+
+            // create the inbox object and open it
+            inbox = store.getFolder("Inbox");
+            inbox.open(Folder.READ_WRITE);
+            messages = inbox.search(new FlagTerm(new Flags(Flags.Flag.SEEN), false));
+            System.out.println(formatter.format(Calendar.getInstance().getTime())+":-messages.length---" + messages.length);
+            if(messages.length!=0){
+                Message message = messages[messages.length-1];
+                try {
+                    toEmail=message.getFrom()[0].toString().toLowerCase(Locale.ROOT);
+                    subject=message.getSubject().toLowerCase(Locale.ROOT);
+                    System.out.println(subject);
+                    if(toEmail.contains("santoshkotakonda@gmail.com")){
+                        if(!subject.contains("south")&&!subject.contains("re:")){
+                            if(subject.contains("stockdale")||subject.contains("thomaston")){
+                                sendEmail(message);
+                            }else{
+                                String content=getTextFromMessage(message).toLowerCase(Locale.ROOT);
+                                System.out.println("Text: " + content);
+                                if(!content.contains("south") &&(content.contains("stockdale")||content.contains("thomaston"))){
+                                    sendEmail(message);
+                                }
+                            }
+                        }
+                    }
+                    message.setFlag(Flags.Flag.SEEN, true);
+                }catch (IOException exception){
+                }
+            }
+           inbox.close(false);
+        }catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+
+    public  String getTextFromMessage(Message message) throws MessagingException, IOException {
+        String result = "";
+        if (message.isMimeType("text/plain")) {
+            result = message.getContent().toString();
+        } else if (message.isMimeType("multipart/*")) {
+            MimeMultipart mimeMultipart = (MimeMultipart) message.getContent();
+            result = getTextFromMimeMultipart(mimeMultipart);
+        }
+        return result;
+    }
+    private  void sendEmail( Message message){
+
+        try{
+            MimeMessage mimeMessage = new MimeMessage(session);
+            mimeMessage=(MimeMessage) mimeMessage.reply(false);
+            // Set From: header field of the header.
+            mimeMessage.setFrom(new InternetAddress(from));
+            // Set To: header field of the header.
+            mimeMessage.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(toEmail));
+            //mimeMessage.setReplyTo(message.getReplyTo());
+            // Set Subject: header field
+            mimeMessage.setSubject("RE: "+ message.getSubject());
+            BodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setText("Interested");
+            // Create a multi-part to combine the parts
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(messageBodyPart);
+            // Create and fill part for the forwarded content
+            messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setDataHandler(message.getDataHandler());
+            // Add part to multi part
+            multipart.addBodyPart(messageBodyPart);
+            // Associate multi-part with message
+            mimeMessage.setContent(multipart);
+            // Send message
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, from, pass);
+            transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
+            transport.close();
+            replyToEmail.sendEmailStatus=true;
+            System.out.println("Email sent time:- "+formatter.format(Calendar.getInstance().getTime()));
+        }catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+    private String getTextFromMimeMultipart(
+            MimeMultipart mimeMultipart)  throws MessagingException, IOException{
+        String result = "";
+        int count = mimeMultipart.getCount();
+        for (int i = 0; i < count; i++) {
+            BodyPart bodyPart = mimeMultipart.getBodyPart(i);
+            if (bodyPart.isMimeType("text/plain")) {
+                result = result + "\n" + bodyPart.getContent();
+                break; // without break same text appears twice in my tests
+            } else if (bodyPart.isMimeType("text/html")) {
+                String html = (String) bodyPart.getContent();
+                result = result + "\n" + org.jsoup.Jsoup.parse(html).text();
+            } else if (bodyPart.getContent() instanceof MimeMultipart){
+                result = result + getTextFromMimeMultipart((MimeMultipart)bodyPart.getContent());
+            }
+        }
+        return result;
+    }
+
+}
