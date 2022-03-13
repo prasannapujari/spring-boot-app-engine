86c60c7ab3fdc78c9655b83e4d9c3b1395ee5ac7
prasannapujari <prasanna.pujari66@gmail.com>
2022-03-13 00:35:53 -0500

    logger

--- src/main/java/ro/robertsicoie/appengine/ReplyToEmail.java
+++ src/main/java/ro/robertsicoie/appengine/ReplyToEmail.java
@@ -11,8 +11,11 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 public class ReplyToEmail {
+
+    private static final Logger logger = Logger.getLogger("ReplyToEmail");
     public  boolean sendEmailStatus=false;
     public SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss.SSS");
     public static String from = "prasanna.pujari66@gmail.com";
@@ -73,7 +76,7 @@ public class ReplyToEmail {
             inbox = store.getFolder("Inbox");
             inbox.open(Folder.READ_WRITE);
             messages = inbox.search(new FlagTerm(new Flags(Flags.Flag.SEEN), false));
-            System.out.println(formatter.format(Calendar.getInstance().getTime())+":-messages.length---" + messages.length);
+            logger.info(formatter.format(Calendar.getInstance().getTime())+":-messages.length---" + messages.length);
             if(messages.length!=0){
                 Message message = messages[messages.length-1];
                 try {
@@ -144,7 +147,7 @@ public class ReplyToEmail {
             transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
             transport.close();
             replyToEmail.sendEmailStatus=true;
-            System.out.println("Email sent time:- "+formatter.format(Calendar.getInstance().getTime()));
+            logger.info("Email sent time:- "+formatter.format(Calendar.getInstance().getTime()));
         }catch (MessagingException mex) {
             mex.printStackTrace();
         }
